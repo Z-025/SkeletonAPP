391ebc8906b33495da3200a8af78461f76001ae8
Pamela Azúa <pam.azua@duocuc.cl>
2024-05-19 23:06:03 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'access',
+    loadChildren: () => import('./access/access.module').then( m => m.AccessPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
